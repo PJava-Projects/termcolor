237b3d8ddd08349754aa046e0069cecf076ee3e5
Patrick Chan <patrickchan1999@gmail.com>
2019-03-01 22:57:15 -0500

    Add methods for manipulating cursor location and for clearing content

---
+++ Movement.java
@@ -0,0 +1,71 @@
+package termcolor;
+
+/**
+ * This class provides methods for manipulating the cursor location and clearing parts
+ * of the terminal contents.
+ */
+public class Movement {
+
+    /**
+     * Clear the screen and move the cursor to the top left.
+     */
+    public static void clearScreen() {
+        System.out.printf("\033[2J");
+        System.out.flush();
+    }
+
+    /**
+     * Clear the rest of the line that the cursor is on.
+     */
+    public static void clearRestOfLine() {
+        System.out.printf("\033[K");
+        System.out.flush();
+    }
+
+    /**
+     * Move the cursor up n rows.
+     * @param numTimes the number of rows to move up by
+     */
+    public static void cursorUp(int numTimes) {
+        System.out.printf("\033[%dA", numTimes);
+        System.out.flush();
+    }
+
+    /**
+     * Move the cursor down n rows.
+     * @param numTimes the number of rows to move down by
+     */
+    public static void cursorDown(int numTimes) {
+        System.out.printf("\033[%dB", numTimes);
+        System.out.flush();
+    }
+
+    /**
+     * Move the cursor left n columns.
+     * @param numTimes the number of columns to move left by
+     */
+    public static void cursorLeft(int numTimes) {
+        System.out.printf("\033[%dD", numTimes);
+        System.out.flush();
+    }
+
+    /**
+     * Move the cursor right n columns.
+     * @param numTimes the number of columns to move right by
+     */
+    public static void cursorRight(int numTimes) {
+        System.out.printf("\033[%dC", numTimes);
+        System.out.flush();
+    }
+
+    /**
+     * Move the cursor to a specific row and column.
+     * @param x the row to move the cursor to, starts with 0.
+     * @param y the column to move the cursor to, starts with 0.
+     */
+    public static void cursorTo(int x, int y) {
+        System.out.printf("\033[%d;%dH", x, y);
+        System.out.flush();
+    }
+    
+}
